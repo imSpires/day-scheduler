3a4c10cb8ce3238d19f44a1bb606c9a3ef6f3adf
Ryan Dungan <rd3@knights.ucf.edu>
2021-07-26 11:48:24 -0400

    added color to webpage for past/future/present

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,33 @@ function updateClock() {
 // Call function
 updateClock();
 
+// Function to check the current hour and change background color
+// of task depending on hour
+
+function hourColor() {
+    // Run hourComparison for each hour of work day
+    $(".hour-container").each(hourComparison);
+}
+
+// Function to compare each hour of work day to the current hour
+function hourComparison() {
+    // Get current hour using moment.js
+    // Moment().hour() ranges from 0-23
+    // https://www.geeksforgeeks.org/moment-js-moment-hour-method/
+    var currentHour = moment().hour();
+
+    // Get the numbered hour of each
+    var workDayHour = parseInt($(this).attr("id").split("workdayhour")[1]);
+
+    if (currentHour > workDayHour) {
+        $(this).addClass("past");
+    } else if (currentHour < workDayHour) {
+        $(this).addClass("future");
+    } else {
+        $(this).addClass("present");
+    }
+}
+
 // Function to save the users input to the text area
 function saveText() {
     var textContent = $(this).siblings(".text-content").val();
@@ -21,3 +48,6 @@ function saveText() {
 
 // Call saveText on save button click
 $(".saveBtn").on("click", saveText);
+
+//Call all functions
+hourColor();
