1b88d792135d0d48788bc991993633e8ddc7cc36
Ryan Dungan <rd3@knights.ucf.edu>
2021-07-25 23:13:16 -0400

    save button clicks to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,23 @@
-var currentDayEl = document.querySelector("#currentDay");
-var currentTimeEl = document.querySelector("#currentTime");
+// Variables
+var now = moment();
+
+// Function to update the current date/time in header
+function updateClock() {
+    currentTime = now.format("LLLL")
+    $("#currentTime").html(currentTime);
+}
+
+// Call function
+updateClock();
+
+// Function to save the users input to the text area
+function saveText() {
+    var textContent = $(this).siblings(".text-content").val();
+    var hour = $(this).parent().attr("id");
+
+    // Save to local storage
+    localStorage.setItem(hour, textContent);
+}
+
+// Call saveText on save button click
+$(".saveBtn").on("click", saveText);
